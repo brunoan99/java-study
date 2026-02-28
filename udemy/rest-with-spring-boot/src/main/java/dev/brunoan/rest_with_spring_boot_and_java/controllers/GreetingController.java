9df79e472f6386febfcdc0abe463ba65b42e3af9
Bruno Andrade <brunoan99@gmail.com>
2026-02-28 19:19:25 -0300

    feat(udemy): add Model and Controller to Greeting

---
+++ udemy/rest-with-spring-boot/src/main/java/dev/brunoan/rest_with_spring_boot_and_java/controllers/GreetingController.java
@@ -0,0 +1,21 @@
+package dev.brunoan.rest_with_spring_boot_and_java.controllers;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import dev.brunoan.rest_with_spring_boot_and_java.model.Greeting;
+
+@RestController
+public class GreetingController {
+
+  private static final String template = "Hello, %s!";
+  private final AtomicLong counter = new AtomicLong(0);
+
+  @RequestMapping("/greeting")
+  public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+    return new Greeting(counter.incrementAndGet(), String.format(template, name));
+  }
+}
