3366a93763e18093ef8415723728c9eae7229f85
Bruno Andrade <brunoan99@gmail.com>
2026-03-09 02:23:43 -0300

    feat(udemy): add TestLogController to test log levels

---
+++ udemy/rest-with-spring-boot/src/main/java/dev/brunoan/rest_with_spring_boot_and_java/controllers/TestLogController.java
@@ -0,0 +1,22 @@
+package dev.brunoan.rest_with_spring_boot_and_java.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestLogController {
+
+  private Logger logger = LoggerFactory.getLogger(TestLogController.class.getName());
+
+  @GetMapping("/test-log")
+  public String testLog() {
+    logger.debug("Test log message with debug level");
+    logger.info("Test log message with info level");
+    logger.warn("Test log message with warn level");
+    logger.error("Test log message with error level");
+    return "Logs generated succesfully!";
+  }
+}
