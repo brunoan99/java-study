fd87eb1b4678901cb2bd2f3617702f5c41f8a535
Bruno Andrade <brunoan99@gmail.com>
2026-03-12 06:10:53 -0300

    feat(udemy): get application configs from git repository and serve them using Config Server

---
+++ spring/spring-cloud-config-server/src/main/java/dev/brunoan/springcloudconfigserver/Startup.java
@@ -0,0 +1,15 @@
+package dev.brunoan.springcloudconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class Startup {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Startup.class, args);
+  }
+
+}
