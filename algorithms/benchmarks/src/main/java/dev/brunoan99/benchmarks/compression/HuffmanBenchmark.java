1869ce5fe18fbd811a4a6f41935a9ff4fb916622
Bruno Andrade <brunoan99@gmail.com>
2026-03-21 08:27:49 -0300

    refactor(algorithms): apply utilities of BenchmarkRunner to HuffmanBenchmark

--- algorithms/benchmarks/src/main/java/dev/brunoan99/benchmarks/compression/HuffmanBenchmark.java
+++ algorithms/benchmarks/src/main/java/dev/brunoan99/benchmarks/compression/HuffmanBenchmark.java
@@ -1,159 +1,172 @@
 package dev.brunoan99.benchmarks.compression;
 
 import dev.brunoan99.algorithms.compression.Huffman;
-import dev.brunoan99.utilities.Table;
+import dev.brunoan99.utilities.BenchmarkRunner;
+import dev.brunoan99.utilities.RandomInputHelper;
+import dev.brunoan99.utilities.ResultAggregator;
 
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.HashMap;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 public class HuffmanBenchmark {
   private HuffmanBenchmark() {
   }
 
-  private record RandomTestConfig(int randomTestsNumber, int randomStringLength, int maxSequenceLength) {
+  record ResultLine(
+      int originalSize,
+      int compressedSize,
+      long initializingTime,
+      long encodingTime,
+      long decodingTime) {
   }
 
-  private record ProcessingResult(int inputSize, int compressedSize, double compressionRatio, double initializingTime,
-      double encodingTime,
-      double decodingTime) {
+  record ResultFinal(
+      int count,
+      float meanOriginalSize,
+      float meanCompressedSize,
+      float meanCompressingRatio,
+      float meanInitializingTime,
+      float meanEncodingTime,
+      float meanDecodingTime) {
   }
 
-  private record RandomTestResults(RandomTestConfig config, double meanCompressionRatio,
-      double meanInitilizeTime,
-      double meanEncodingTime,
-      double meanDecodingTime) {
+  public static class HuffmanBenchmarkAccumulator implements ResultAggregator.Accumulator<ResultLine, ResultFinal> {
+    int count = 0;
+    float sumOriginalSize = 0L;
+    float sumCompressedSize = 0L;
+    float sumCompressingRatio = 0L;
+    float sumInitializingTime = 0L;
+    float sumEncodingTime = 0L;
+    float sumDecodingTime = 0L;
+
+    @Override
+    public void add(ResultLine v) {
+      count++;
+      sumOriginalSize += v.originalSize;
+      sumCompressedSize += v.compressedSize;
+      sumCompressingRatio += ((float) v.compressedSize / (float) v.originalSize);
+      sumInitializingTime += v.initializingTime;
+      sumEncodingTime += v.encodingTime;
+      sumDecodingTime += v.decodingTime;
+    }
+
+    @Override
+    public ResultFinal result() {
+      return new ResultFinal(
+          count,
+          sumOriginalSize / count,
+          sumCompressedSize / count,
+          sumCompressingRatio / count,
+          sumInitializingTime / count,
+          sumEncodingTime / count,
+          sumDecodingTime / count);
+    }
   }
 
-  private static RandomTestResults generateAndProcess(RandomTestConfig config) {
-    String[] characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz".split("");
-    List<ProcessingResult> results = new ArrayList<>();
-    for (int i = 0; i < config.randomTestsNumber(); i++) {
-      StringBuilder sb = new StringBuilder();
-      while (sb.length() < config.randomStringLength()) {
-        int remaining = config.randomStringLength() - sb.length();
-        int maxSeq = Math.min(config.maxSequenceLength(), remaining);
-        int seqLen = (int) (Math.random() * maxSeq) + 1;
-        sb.append(characters[(int) (Math.random() * characters.length)].repeat(seqLen));
-      }
-      String input = sb.toString();
+  private static ResultLine processFunction(RandomInputHelper.InputLine inputLine) {
+    try {
+
+      String text = inputLine.value();
 
       long initializingStartTime = System.nanoTime();
-      Huffman huffman = new Huffman(input);
+      Huffman huffman = new Huffman(text);
       long initializingEndTime = System.nanoTime();
       long initializingTime = initializingEndTime - initializingStartTime;
 
       long encodingStartTime = System.nanoTime();
-      String encoded = huffman.encode(input);
+      String encoded = huffman.encode(text);
       long encodingEndTime = System.nanoTime();
       long encodingTime = encodingEndTime - encodingStartTime;
 
-      long decondingStartTime = System.nanoTime();
+      long decodingStartTime = System.nanoTime();
       String decoded = huffman.decode(encoded);
-      long decondingEndTime = System.nanoTime();
-      long decondingTime = decondingEndTime - decondingStartTime;
+      long decodingEndTime = System.nanoTime();
+      long decodingTime = decodingEndTime - decodingStartTime;
 
-      double compressedRatio = (double) encoded.length() / (input.length() * 8);
-
-      if (!input.equals(decoded)) {
-        throw new RuntimeException("Decompressed string does not match original");
+      if (!text.equals(decoded)) {
+        IO.println("------------------------------");
+        IO.println("Original: " + text);
+        IO.println("Decoded: " + decoded);
+        IO.println("------------------------------");
+        throw new RuntimeException("Decoded string does not match original");
       }
-      results
-          .add(new ProcessingResult(input.length() * 8, encoded.length(), compressedRatio, initializingTime,
-              encodingTime,
-              decondingTime));
+      return new ResultLine(
+          text.length(),
+          encoded.length(),
+          initializingTime,
+          encodingTime,
+          decodingTime);
+    } catch (IllegalArgumentException e) {
+      IO.println("------------------------------");
+      IO.println("Original: " + inputLine.value());
+      IO.println("------------------------------");
+      throw e;
     }
-    double meanCompressionRatio = results.stream()
-        .mapToDouble(ProcessingResult::compressionRatio)
-        .average()
-        .orElse(0.0);
-    double meanInitializingTime = results.stream()
-        .mapToDouble(ProcessingResult::initializingTime)
-        .average()
-        .orElse(0.0);
-    double meanEncodingTime = results.stream()
-        .mapToDouble(ProcessingResult::encodingTime)
-        .average()
-        .orElse(0.0);
-    double meanDecodingTime = results.stream()
-        .mapToDouble(ProcessingResult::decodingTime)
-        .average()
-        .orElse(0.0);
-    return new RandomTestResults(config, meanCompressionRatio, meanInitializingTime, meanEncodingTime,
-        meanDecodingTime);
   }
 
-  private static ArrayList<String> process(RandomTestConfig config) {
-    RandomTestResults result = generateAndProcess(config);
-    return new ArrayList<String>(Arrays.asList(
-        String.valueOf(config.randomStringLength()),
-        String.valueOf(config.maxSequenceLength()),
-        String.valueOf(config.randomTestsNumber()),
-        String.format("%.15f", result.meanCompressionRatio()),
-        String.format("%.0f", result.meanInitilizeTime()),
-        String.format("%.0f", result.meanEncodingTime()),
-        String.format("%.0f", result.meanDecodingTime())));
-  }
-
-  public static void benchmarkRandomTests() throws Exception {
+  private static ArrayList<ArrayList<String>> formatFunction(HashMap<BenchmarkRunner.InputParam, ResultFinal> resMap) {
     ArrayList<ArrayList<String>> table = new ArrayList<ArrayList<String>>();
-
     table.add(new ArrayList<String>(
-        Arrays.asList("String Length",
+        Arrays.asList(
+            "String Length",
             "Max Sequence Length",
             "Tests Number",
             "Mean Compression Ratio",
-            "Mean Initialize Time (ns)",
-            "Mean Encode Time (ns)",
-            "Mean Decode Time (ns)")));
-
-    int randomTestsNumber = 1_000;
-
-    int minRandomStringLength = 64; // 64B
-    int maxRandomStringLength = 67_108_864; // 64MB
-
-    int minMaxSequenceLength = 4; // 4B
-    int maxMaxSequenceLength = 65_536; // 64KB
-
-    ExecutorService pool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
-    List<Callable<ArrayList<String>>> tasks = new ArrayList<>();
-
-    for (int randomStringLength = minRandomStringLength; randomStringLength <= maxRandomStringLength; randomStringLength *= 2) {
-      for (int maxSequenceLength = minMaxSequenceLength; maxSequenceLength <= maxMaxSequenceLength; maxSequenceLength *= 2) {
-        if (maxSequenceLength < randomStringLength / 262_144) {
-          continue;
-        }
-        if (maxSequenceLength > randomStringLength / 4) {
-          break;
-        }
-        final RandomTestConfig config = new RandomTestConfig(randomTestsNumber, randomStringLength, maxSequenceLength);
-        tasks.add(() -> process(config));
-      }
-    }
-
-    for (Future<ArrayList<String>> future : pool.invokeAll(tasks)) {
-      table.add(future.get());
-    }
-    pool.shutdown();
-
-    String formattedTable = Table.formatTable(table);
-    System.out.println(formattedTable);
-    try {
-      long timestamp = System.currentTimeMillis();
-      String folder = "../benchmarks/benchmarks_results/compression/huffman";
-      String filename = folder + "/huffman_encoding_random_tests_results_" + timestamp + ".txt";
-      FileWriter writer = new FileWriter(filename);
-      writer.write(formattedTable);
-      writer.close();
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
+            "Mean Initializing Time (ns)",
+            "Mean Encoding Time (ns)",
+            "Mean Decoding Time (ns)")));
+    resMap.entrySet().stream()
+        .sorted(java.util.Comparator
+            .comparingInt(
+                (java.util.Map.Entry<BenchmarkRunner.InputParam, ResultFinal> e) -> e.getKey().randomStringLength())
+            .thenComparingInt(e -> e.getKey().maxSequenceLength()))
+        .forEach(entry -> {
+          BenchmarkRunner.InputParam input = entry.getKey();
+          ResultFinal resfinal = entry.getValue();
+          table.add(new ArrayList<String>(
+              Arrays.asList(
+                  String.valueOf(input.randomStringLength()),
+                  String.valueOf(input.maxSequenceLength()),
+                  String.valueOf(resfinal.count),
+                  String.format("%.10f", resfinal.meanCompressingRatio),
+                  String.format("%,.0f", resfinal.meanInitializingTime),
+                  String.format("%,.0f", resfinal.meanEncodingTime),
+                  String.format("%,.0f", resfinal.meanDecodingTime))));
+        });
+    return table;
   }
 
+  public static void benchmarkRandomTestsByHelper(boolean logOnConsole, boolean saveFile) throws Exception {
+    BenchmarkRunner.BenchmarkConfig benchConfig = new BenchmarkRunner.BenchmarkConfig(
+        32,
+        512,
+        1,
+        32,
+        1000);
+
+    long timestamp = System.currentTimeMillis();
+    String folder = "../benchmarks/benchmarks_results/compression/rle/";
+    String path = folder + "rle_compressor_random_tests_results_" + timestamp + ".txt";
+
+    BenchmarkRunner.GeneralConfig config = new BenchmarkRunner.GeneralConfig(
+        benchConfig,
+        logOnConsole,
+        saveFile,
+        path);
+
+    BenchmarkRunner benchRunner = new BenchmarkRunner(config);
+
+    Supplier<ResultAggregator.Accumulator<ResultLine, ResultFinal>> accumulatorFactory = () -> new HuffmanBenchmarkAccumulator();
+    Function<RandomInputHelper.InputLine, ResultLine> processFunction = inputLine -> processFunction(inputLine);
+    Function<HashMap<BenchmarkRunner.InputParam, ResultFinal>, ArrayList<ArrayList<String>>> formatFunction = resMap -> formatFunction(
+        resMap);
+
+    benchRunner.benchmarkRandomTest(
+        accumulatorFactory,
+        processFunction,
+        formatFunction);
+  }
 }
